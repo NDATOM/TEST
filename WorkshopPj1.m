fff1fecc0fd887196a09cef15875586e03ccd532
이수현 <suhyun22@ajou.ac.kr>
2022-04-16 23:48:27 +0900

    suhyuncommit 최종 04162348 구조체, 자료형 변환, 스위치

--- WorkshopPj1.m
+++ WorkshopPj1.m
@@ -1,33 +1,24 @@
 askKorea="한국 원화 단위 얼마 있으신가요?";
 x = input(askKorea);
+country=struct;
+country.name=["미국","유럽","일본","중국"];
+country.exchangeRate=[0.00081, 1328.88 , 9.72,192.91];
+country.moneyName=[" 달러"," 유로"," 엔"," 위안"];
 
-% while 1%fgfgfgfg
-%     askCountry="환전하려는 국가는 어디신가요? (1=미국, 2=유럽, 3=일본 4=중국)";
-%     y= input(askCountry);
-%     if (y>=1 && y<=4 && (y-int8(y)==0))
-%         break;
-%     end
-%     disp("(1=미국, 2=유럽, 3=일본 4=중국) 중에 선택해주세요");
-% end
+exchange(x,country);
 
-% 1미국 0.00081  2유럽 1328.88 3일본 9.72 4중국 192.91
-
-exchange(x);
-
-function money=exchange(x)
-    exchangeRate=[0.00081, 1328.88 , 9.72,192.91];
-    exchangeCountry=["미국","유럽","일본","중국"];
-    exchangeName=[" 달러"," 유로"," 엔"," 위안"];
+function money=exchange(x,country)
     for i = 1:1:4
-          money=x*exchangeRate(i);
-          disp(exchangeCountry(i)+" 총 : "+money+exchangeName(i)+" 입니다");
+          money=x*country.exchangeRate(i);
+          disp(country.name(i)+" 총 : "+money+country.moneyName(i)+" 입니다");
           moneyPaper(i, money)
     end
 end
 
 function moneyReturn=moneyPaper(countryNum,moneyTotal)
+ countryNum=int8(countryNum);
  switch countryNum
-    case '1'
+    case 1
        sum = 0;
        sum = sum + fix(moneyTotal/100);
        r = rem(moneyTotal,100);
@@ -42,8 +33,8 @@ function moneyReturn=moneyPaper(countryNum,moneyTotal)
        sum = sum + fix(r/2);
        r = rem(r,2);
        sum = sum + fix(r/1);
-       disp(" 총 : "+sum+"의 화폐가 필요합니다");
-    case '2'
+       disp(" 총 : "+sum+" 개의 화폐가 필요합니다");
+    case 2
          sum = 0;
        sum = sum + fix(moneyTotal/500);
        r = rem(moneyTotal,500);
@@ -58,8 +49,8 @@ function moneyReturn=moneyPaper(countryNum,moneyTotal)
        sum = sum + fix(r/10);
        r = rem(r,10);
        sum = sum + fix(r/5);
-       disp(" 총 : "+sum+"의 화폐가 필요합니다");
-    case '3'
+       disp(" 총 : "+sum+" 개의 화폐가 필요합니다");
+    case 3
          sum = 0;
        sum = sum + fix(moneyTotal/10000);
        r = rem(moneyTotal,10000);
@@ -68,8 +59,8 @@ function moneyReturn=moneyPaper(countryNum,moneyTotal)
        sum = sum + fix(r/2000);
        r = rem(r,2000);
        sum = sum + fix(r/1000);
-       disp(" 총 : "+sum+"의 화폐가 필요합니다");
-    case '4'
+       disp(" 총 : "+sum+" 개의 화폐가 필요합니다");
+    case 4
          sum = 0;
        sum = sum + fix(moneyTotal/100);
        r = rem(moneyTotal,100);
@@ -82,7 +73,6 @@ function moneyReturn=moneyPaper(countryNum,moneyTotal)
        sum = sum + fix(r/5);
        r = rem(r,5);
        sum = sum + fix(r/1);
-       disp(" 총 : "+sum+"의 화폐가 필요합니다");
+       disp(" 총 : "+sum+" 개의 화폐가 필요합니다");
  end
-
 end
